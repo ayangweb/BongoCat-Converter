fbe47ee861bc1c3ac94310f7bb6d83ab344064db
ayang <473033518@qq.com>
2025-07-11 14:07:20 +0800

    feat: introduce Vercel Web Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import { ToastProvider } from "@heroui/react";
 import { HeroUIProvider } from "@heroui/system";
+import { Analytics } from "@vercel/analytics/react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
 import "@/styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <HeroUIProvider>
-    <ToastProvider />
-    <App />
-  </HeroUIProvider>,
+  <>
+    <Analytics />
+
+    <HeroUIProvider>
+      <ToastProvider />
+      <App />
+    </HeroUIProvider>
+  </>,
 );
